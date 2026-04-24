2964ba1df07804b123e8171a5ac7b1a48f896e04
Milan <deepanshu.v7@gmail.com>
2026-04-24 21:02:30 +0530

    added terraform code for linux vm

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,8 +5,17 @@ terraform {
       version = "~>3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "my-rg"
+    storage_account_name = "tfstate12345demomilan245"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
 }
 
 provider "azurerm" {
   features {}
 }
+
