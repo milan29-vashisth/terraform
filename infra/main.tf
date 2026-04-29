818bd8630cc967b73e7cfb9d19271d3aac03abaa
Milan <deepanshu.v7@gmail.com>
2026-04-29 14:34:35 +0530

    minor change

--- infra/main.tf
+++ infra/main.tf
@@ -18,7 +18,7 @@ resource "azurerm_subnet" "subnet" {
 }
 
 resource "azurerm_public_ip" "pip" {
-  name                = "pip-demo-${var.environment}"
+  name                = "pip-demo-again-${var.environment}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
