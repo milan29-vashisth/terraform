f5c91b971114b90110bbf4c9ee9a1c3302ae7576
Milan <deepanshu.v7@gmail.com>
2026-04-30 15:47:16 +0530

    commit for diffrent

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "pip" {
   name                = "pip-demo-${var.environment}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "nic" {
